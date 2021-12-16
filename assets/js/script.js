79502cc5261bbe289f21f59bfd75bcd64447d21d
samuellgc <samuellgc@hotmail.com>
2021-12-16 18:48:32 -0300

    Adição dos arquivos HTML, CSS e JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function getTimeFromSeconds(seconds) {
+    const data = new Date(seconds * 1000);
+    return data.toLocaleTimeString('pt-BR',{
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let seconds = 0;
+let timer;
+let reset
+
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        seconds++;
+        relogio.innerHTML = getTimeFromSeconds(seconds);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function(e) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+pausar.addEventListener('click', function(e) {
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+zerar.addEventListener('click', function(e) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00'
+    seconds = 0;
+});
